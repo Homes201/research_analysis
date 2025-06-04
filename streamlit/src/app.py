2fb0fda35a00964156af1a27da73fe220922c07c
sangjip <tkdwlq11@naver.com>
2025-06-05 07:27:20 +0900

    app.py 26번 label name 수정

--- streamlit/src/app.py
+++ streamlit/src/app.py
@@ -710,11 +710,11 @@ for q_num in list(range(1, 12)) + [111] + list(range(12, 32)):
     elif q_num == 26:
         col = 'impact_on_company_brand_image'
         label_map = {
-            1: '매우 부정적 영향',
-            2: '다소 부정적 영향',
-            3: '영향 없음(무관함)',
-            4: '다소 긍정적 영향',
-            5: '매우 긍정적 영향'
+            1: '전혀 없음',
+            2: '낮음',
+            3: '보통',
+            4: '높음',
+            5: '매우 높음'
         }
         order = [1, 2, 3, 4, 5]
         labels = [label_map[i] for i in order]
